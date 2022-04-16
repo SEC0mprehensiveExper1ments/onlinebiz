0ccbc395a858050d8de75a8ea739d1b70e316b0c
YiyangSunn <1196168404@qq.com>
2022-04-16 00:29:15 +0000

    初始化项目

---
+++ doc-service/src/main/java/com/njustc/onlinebiz/microdoc/DocumentServiceApplication.java
@@ -0,0 +1,11 @@
+package com.njustc.onlinebiz.microdoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DocumentServiceApplication.class, args);
+    }
+}
