8040c02161ba9a4bc90e1fee525caf46167c0894
syhien <syhien@outlook.at>
2022-04-20 22:23:47 +0800

    格式化代码

--- onlinebiz-document/src/main/java/com/njustc/onlinebiz/doc/domain/JS003.java
+++ onlinebiz-document/src/main/java/com/njustc/onlinebiz/doc/domain/JS003.java
@@ -5,72 +5,80 @@ import lombok.Data;
 import java.util.List;
 
 @Data
-class ZiGongNeng{
-    private String inputZiGongNengMingCheng;
-    private String inputGongNengShuoMing;
+class ZiGongNeng {
+  private String inputZiGongNengMingCheng;
+  private String inputGongNengShuoMing;
 
-    public String getInputZiGongNengMingCheng() {
-        return inputZiGongNengMingCheng;
-    }
+  public String getInputZiGongNengMingCheng() {
+    return inputZiGongNengMingCheng;
+  }
 
-    public String getInputGongNengShuoMing() {
-        return inputGongNengShuoMing;
-    }
+  public String getInputGongNengShuoMing() {
+    return inputGongNengShuoMing;
+  }
 }
 
 @Data
-class GongNeng{
-    private String inputGongNengMingCheng;
-    private List<ZiGongNeng> inputZiGongNeng;
+class GongNeng {
+  private String inputGongNengMingCheng;
+  private List<ZiGongNeng> inputZiGongNeng;
 
-    public String getInputGongNengMingCheng() {
-        return inputGongNengMingCheng;
-    }
+  public String getInputGongNengMingCheng() {
+    return inputGongNengMingCheng;
+  }
 
-    public List<ZiGongNeng> getInputZiGongNeng() {
-        return inputZiGongNeng;
-    }
+  public List<ZiGongNeng> getInputZiGongNeng() {
+    return inputZiGongNeng;
+  }
 
-    public int getZiGongNengSum(){
-        return inputZiGongNeng.size();
-    }
+  public int getZiGongNengSum() {
+    return inputZiGongNeng.size();
+  }
 }
 
 @Data
 public class JS003 {
-    private String inputRuanJianMingCheng;
-    private String inputBanBenHao;
-    private List<GongNeng> inputRuanJianGongNengXiangMu;
+  private String inputRuanJianMingCheng;
+  private String inputBanBenHao;
+  private List<GongNeng> inputRuanJianGongNengXiangMu;
 
-    public String getInputRuanJianMingCheng() {
-        return inputRuanJianMingCheng;
-    }
+  public String getInputRuanJianMingCheng() {
+    return inputRuanJianMingCheng;
+  }
 
-    public String getInputBanBenHao() {
-        return inputBanBenHao;
-    }
+  public String getInputBanBenHao() {
+    return inputBanBenHao;
+  }
 
-    public List<GongNeng> getInputRuanJianGongNengXiangMu() {
-        return inputRuanJianGongNengXiangMu;
-    }
+  public List<GongNeng> getInputRuanJianGongNengXiangMu() {
+    return inputRuanJianGongNengXiangMu;
+  }
 
-    public int getGongNengSum(){
-        return inputRuanJianGongNengXiangMu.size();
-    }
+  public int getGongNengSum() {
+    return inputRuanJianGongNengXiangMu.size();
+  }
 
-    public int getZiGongNengSum(int index){
-        return inputRuanJianGongNengXiangMu.get(index).getZiGongNengSum();
-    }
+  public int getZiGongNengSum(int index) {
+    return inputRuanJianGongNengXiangMu.get(index).getZiGongNengSum();
+  }
 
-    public String getGongNengName(int index){
-        return inputRuanJianGongNengXiangMu.get(index).getInputGongNengMingCheng();
-    }
+  public String getGongNengName(int index) {
+    return inputRuanJianGongNengXiangMu.get(index).getInputGongNengMingCheng();
+  }
 
-    public String getZiGongNengName(int index,int j){
-        return inputRuanJianGongNengXiangMu.get(index).getInputZiGongNeng().get(j).getInputZiGongNengMingCheng();
-    }
+  public String getZiGongNengName(int index, int j) {
+    return inputRuanJianGongNengXiangMu
+        .get(index)
+        .getInputZiGongNeng()
+        .get(j)
+        .getInputZiGongNengMingCheng();
+  }
 
-    public String getZiGongNengShuoMing(int index,int j){
-        return inputRuanJianGongNengXiangMu.get(index).getInputZiGongNeng().get(j).getInputGongNengShuoMing();
-    }
+  public String getZiGongNengShuoMing(int index, int j) {
+    return inputRuanJianGongNengXiangMu
+        .get(index)
+        .getInputZiGongNeng()
+        .get(j)
+        .getInputGongNengShuoMing();
+  }
 }
--- onlinebiz-document/src/main/java/com/njustc/onlinebiz/doc/domain/JS004.java
+++ onlinebiz-document/src/main/java/com/njustc/onlinebiz/doc/domain/JS004.java
@@ -4,39 +4,39 @@ import lombok.Data;
 
 @Data
 public class JS004 {
-    private String xiangMuMingCheng;
-    private String weiTuoFangJiaFang;
-    private String shouTuoFangYiFang;
-    private String qianDingDiDian;
-    private String qianDingRiQi_Nian;
-    private String qianDingRiQi_Yue;
-    private String qianDingRiQi_Ri;
+  private String xiangMuMingCheng;
+  private String weiTuoFangJiaFang;
+  private String shouTuoFangYiFang;
+  private String qianDingDiDian;
+  private String qianDingRiQi_Nian;
+  private String qianDingRiQi_Yue;
+  private String qianDingRiQi_Ri;
 
-    public String getXiangMuMingCheng() {
-        return xiangMuMingCheng;
-    }
+  public String getXiangMuMingCheng() {
+    return xiangMuMingCheng;
+  }
 
-    public String getWeiTuoFangJiaFang() {
-        return weiTuoFangJiaFang;
-    }
+  public String getWeiTuoFangJiaFang() {
+    return weiTuoFangJiaFang;
+  }
 
-    public String getShouTuoFangYiFang() {
-        return shouTuoFangYiFang;
-    }
+  public String getShouTuoFangYiFang() {
+    return shouTuoFangYiFang;
+  }
 
-    public String getQianDingDiDian() {
-        return qianDingDiDian;
-    }
+  public String getQianDingDiDian() {
+    return qianDingDiDian;
+  }
 
-    public String getQianDingRiQi_Nian() {
-        return qianDingRiQi_Nian;
-    }
+  public String getQianDingRiQi_Nian() {
+    return qianDingRiQi_Nian;
+  }
 
-    public String getQianDingRiQi_Yue() {
-        return qianDingRiQi_Yue;
-    }
+  public String getQianDingRiQi_Yue() {
+    return qianDingRiQi_Yue;
+  }
 
-    public String getQianDingRiQi_Ri() {
-        return qianDingRiQi_Ri;
-    }
+  public String getQianDingRiQi_Ri() {
+    return qianDingRiQi_Ri;
+  }
 }
