4daa740a6d5a61b5b579833b908a7cd6ed94bcc1
Xianhao-Dai <438081108@qq.com>
2022-05-15 15:24:47 +0800

    006文件增删改查

---
+++ onlinebiz-test/src/main/java/com/example/onlinebiztest/controller/TestController.java
@@ -0,0 +1,108 @@
+package com.example.onlinebiztest.controller;
+
+import com.example.onlinebiztest.service.TestService;
+import com.njustc.onlinebiz.common.model.Scheme;
+import com.njustc.onlinebiz.common.model.Outline;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Slf4j
+@RestController
+public class TestController {
+
+    private final TestService testService;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    public TestController(TestService testService) {
+        this.testService = testService;
+    }
+
+    @PostMapping("/test/scheme")
+    public ResponseEntity<Scheme> createScheme(
+            @RequestParam("applyId") Long applyId,
+            @RequestParam("contractId") Long contractId
+    ) {
+        return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED).build();
+    }
+
+    // 查看任意测试方案的详细信息
+    @GetMapping("/test/scheme/{schemeId}")
+    public ResponseEntity<Scheme> getSchemeById(@PathVariable("schemeId") String schemeId) {
+        Scheme scheme = testService.findSchemeById(schemeId);
+        return scheme == null ?
+                ResponseEntity.notFound().build() :
+                ResponseEntity.ok().body(scheme);
+    }
+
+    // 查看用户自己的所有测试方案
+    @GetMapping("/test/scheme/individual")
+    public ResponseEntity<List<Scheme>> getIndividualTests(@RequestParam("userId") Long userId) {
+        List<Scheme> schemes = null;
+        List<Outline> outlines = restTemplate.getForObject("/contract/individual/?id={userId}", List.class);
+        for (Outline outline:outlines
+             ) {
+            schemes.addAll(testService.search().byContractId(outline.getId()).getResult());
+        }
+        return ResponseEntity.ok().body(schemes);
+    }
+
+    // 根据组合条件查询测试方案
+    @GetMapping("/test/scheme/search")
+    public ResponseEntity<List<Scheme>> searchTests(
+            @RequestParam(value = "contactName", required = false) String contactName,
+            @RequestParam(value = "companyName", required = false) String companyName,
+            @RequestParam(value = "representativeName", required = false) String representativeName,
+            @RequestParam(value = "projectName", required = false) String projectName,
+            @RequestParam(value = "targetSoftware", required = false) String targetSoftware
+    ) {
+        // 检查条件是否全空
+        if (contactName == null && companyName == null && representativeName == null &&
+                projectName == null && targetSoftware == null) {
+            return ResponseEntity.badRequest().build();
+        }
+        // null 参数会被忽略
+        Map<String, String> map = new HashMap();
+        map.put("contactName", contactName);
+        map.put("companyName", companyName);
+        map.put("representativeName", representativeName);
+        map.put("projectName", projectName);
+        map.put("targetSoftware", targetSoftware);
+
+        List<Outline> outlines = restTemplate.getForObject("/contract/search", List.class, map);
+        List<Scheme> schemes = null;
+        for (Outline outline:outlines
+        ) {
+            schemes.addAll(testService.search().byContractId(outline.getId()).getResult());
+        }
+        return ResponseEntity.ok().body(schemes);
+    }
+
+    @PostMapping("/test/scheme/{schemeId}")
+    public ResponseEntity<Void> updateSchemeById(
+            @PathVariable("schemeId") String schemeId,
+            @RequestBody Scheme scheme
+    ) {
+        return schemeId.equals(scheme.getId()) ?
+                testService.updateScheme(scheme) ?
+                        ResponseEntity.ok().build() :
+                        ResponseEntity.badRequest().build() :
+                ResponseEntity.badRequest().build();
+    }
+
+    @DeleteMapping("/test/scheme/{schemeId}")
+    public ResponseEntity<Void> deleteSchemeById(@PathVariable("schemeId") String schemeId) {
+        return testService.removeScheme(schemeId) ?
+                ResponseEntity.ok().build() :
+                ResponseEntity.badRequest().build();
+    }
+}
---
+++ onlinebiz-test/src/main/java/com/example/onlinebiztest/service/MongoTestService.java
@@ -0,0 +1,107 @@
+package com.example.onlinebiztest.service;
+
+import com.mongodb.client.result.DeleteResult;
+import com.mongodb.client.result.UpdateResult;
+import com.njustc.onlinebiz.common.model.Scheme;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.BasicQuery;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.data.mongodb.core.query.UpdateDefinition;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Slf4j
+@Service
+public class MongoTestService implements TestService{
+    private final MongoTemplate mongoTemplate;
+
+    public MongoTestService(MongoTemplate mongoTemplate) {
+        this.mongoTemplate = mongoTemplate;
+    }
+
+    @Override
+    public Scheme createScheme(String applyId, String contractId) {
+        Scheme scheme = new Scheme();
+        scheme.setApplyId(applyId);
+        scheme.setContractId(contractId);
+        try {
+            return mongoTemplate.insert(scheme);
+        } catch (Exception e) {
+            log.warn("创建测试方案失败");
+            return null;
+        }
+    }
+
+    @Override
+    public Scheme findSchemeById(String schemeId) {
+        return mongoTemplate.findById(schemeId, Scheme.class);
+    }
+
+    @Override
+    public Scheme findSchemeByIdAndApply(String schemeId, String applyId) {
+        Scheme scheme = findSchemeById(schemeId);
+        if (scheme != null && applyId.equals(scheme.getApplyId())) {
+            // 的确是该申请对应的测试方案
+            return scheme;
+        }
+        return null;
+    }
+
+    @Override
+    public Scheme findSchemeByIdAndContract(String schemeId, String contractId) {
+        Scheme scheme = findSchemeById(schemeId);
+        if (scheme != null && contractId.equals(scheme.getContractId())) {
+            // 的确是该合同对应的测试方案
+            return scheme;
+        }
+        return null;
+    }
+
+    @Override
+    public boolean updateScheme(Scheme scheme) {
+        // ID 不合法有可能抛出异常
+        try {
+            Query query = new BasicQuery("{_id:ObjectId('" + scheme.getId() + "')}");
+            UpdateDefinition updateDefinition = new Update()
+                    .set("modificationList", scheme.getModificationList())
+                    .set("logo", scheme.getLogo())
+                    .set("systemSummary", scheme.getSystemSummary())
+                    .set("documentSummary", scheme.getDocumentSummary())
+                    .set("baseline", scheme.getBaseline())
+                    .set("hardware", scheme.getHardware())
+                    .set("software", scheme.getSoftware())
+                    .set("otherEnvironment", scheme.getOtherEnvironment())
+                    .set("organization", scheme.getOrganization())
+                    .set("participant", scheme.getParticipant())
+                    .set("testLevel", scheme.getTestLevel())
+                    .set("testType", scheme.getTestType())
+                    .set("testCondition", scheme.getTestCondition())
+                    .set("testToBeExecuted", scheme.getTestToBeExecuted())
+                    .set("testSample", scheme.getTestSample());
+            UpdateResult result = mongoTemplate.updateFirst(query, updateDefinition, Scheme.COLLECTION_NAME);
+            return result.wasAcknowledged() && result.getMatchedCount() == 1;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    @Override
+    public boolean removeScheme(String schemeId) {
+        try {
+            Query query = new BasicQuery("{_id:ObjectId('" + schemeId + "')}");
+            DeleteResult result = mongoTemplate.remove(query, Scheme.COLLECTION_NAME);
+            return result.wasAcknowledged() && result.getDeletedCount() == 1;
+        } catch (Exception e) {
+            // in case of invalid schemeId
+            return false;
+        }
+    }
+
+    @Override
+    public ConditionBuilder search() {
+        return new MongoConditionBuilder(mongoTemplate);
+    }
+}
