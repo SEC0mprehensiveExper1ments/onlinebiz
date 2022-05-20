9f2d55a33358156640f28a6e352b1ce4153e5901
YiyangSunn <1196168404@qq.com>
2022-05-20 12:36:28 +0000

    合并分支

---
+++ onlinebiz-test/src/main/java/com/njustc/onlinebiz/test/config/TestConfig.java
@@ -0,0 +1,13 @@
+package com.njustc.onlinebiz.test.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class TestConfig {
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
