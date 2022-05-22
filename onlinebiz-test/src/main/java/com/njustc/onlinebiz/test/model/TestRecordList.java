b765c1722b498fff31ff9d78b6d0ede814c6d20d
Johnny J <1149224994@qq.com>
2022-05-22 13:49:57 +0800

    添加了测试用例表，测试记录表和测试问题清单的model

--- onlinebiz-test/src/main/java/com/njustc/onlinebiz/test/model/TestRecordList.java
+++ onlinebiz-test/src/main/java/com/njustc/onlinebiz/test/model/TestRecordList.java
@@ -9,27 +9,27 @@ import java.util.List;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class Testcase {
+public class TestRecordList {
     // MongoDB 中存放此类型对象的 collection 的名字
-    public static final String COLLECTION_NAME = "testcase";
+    public static final String COLLECTION_NAME = "testRecord";
 
-    // 测试用例表 id，由 MongoDB 自动生成
+    // 软件测试记录表 id，由 MongoDB 自动生成
     private String id;
 
     // 此测试方案对应的委托 id
     private String entrustId;
 
-    //所有测试用例记录
-    private List<TestcaseList> testcases;
+    //所有软件测试记录
+    private List<TestRecord> testRecords;
 
-    //测试用例表状态
-    private TestcaseStatus status;
+    //软件测试记录表状态
+    private TestRecordStatus status;
 
-    //一条测试用例记录
+    //一条软件测试记录
     @Data
     @NoArgsConstructor
     @AllArgsConstructor
-    public static class TestcaseList{
+    public static class TestRecord{
         //测试分类
         private String category;
 
@@ -42,13 +42,34 @@ public class Testcase {
         //与本测试用例有关的规约说明
         private String statute;
 
+        //前提条件
+        private String prerequisites;
+
+        //测试用例执行过程
+        private String executionProcess;
+
         //预期的结果
         private String expectedResult;
 
         //测试用例设计者
         private String designer;
 
-        //测试时间
+        //实际结果
+        private String actualResult;
+
+        //是否与预期结果一致
+        private String isConsistent;
+
+        //相关的BUG编号
+        private String bugId;
+
+        //用例执行者
+        private String caseExecutor;
+
+        //执行测试时间
         private String time;
+
+        //确认人
+        private String confirmationPerson;
     }
 }
