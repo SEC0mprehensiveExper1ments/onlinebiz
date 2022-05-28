cfb3d111c8db8b13b541ce21c455e4684a0054e4
Johnny J <1149224994@qq.com>
2022-05-28 15:55:55 +0800

    基本完成测试报告的service层代码

--- onlinebiz-test/src/main/java/com/njustc/onlinebiz/test/service/report/ReportService.java
+++ onlinebiz-test/src/main/java/com/njustc/onlinebiz/test/service/report/ReportService.java
@@ -4,15 +4,23 @@ import com.njustc.onlinebiz.common.model.Role;
 import com.njustc.onlinebiz.test.model.report.Report;
 
 public interface ReportService {
-  // 生成报告
-  String createReport(Report report, Long userId, Role userRole);
+    // 生成报告
+    String createReport(String projectId, String entrustId, Report.ReportContent content, Long userId, Role userRole);
 
-  // 由 id 查询报告
-  Report findReport(String reportId, Long userId, Role userRole);
+    // 由 id 查询报告
+    Report findReport(String reportId, Long userId, Role userRole);
 
-  // 更新报告
-  void updateReport(String reportId, Report report, Long userId, Role userRole);
+    // 更新报告
+    void updateReport(String reportId, Report.ReportContent content, Long userId, Role userRole);
 
-  // 删除报
-  void deleteReport(String reportId, Long userId, Role userRole);
+    // 删除报告
+    void deleteReport(String reportId, Long userId, Role userRole);
+
+    void QAApprove(String reportId, Long userId, Role userRole);
+
+    void QADeny(String reportId, String message, Long userId, Role userRole);
+
+    void CustomerApprove(String reportId, Long userId, Role userRole);
+
+    void CustomerDeny(String reportId, String message, Long userId, Role userRole);
 }
