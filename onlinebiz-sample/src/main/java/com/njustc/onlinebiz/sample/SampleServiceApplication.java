1cbf87bc1f60fd852a39e0d280a0b82f9cee7368
syhien <syhien@outlook.at>
2022-05-29 22:11:48 +0800

    添加 样品服务

---
+++ onlinebiz-sample/src/main/java/com/njustc/onlinebiz/sample/SampleServiceApplication.java
@@ -0,0 +1,11 @@
+package com.njustc.onlinebiz.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleServiceApplication {
+  public void main(String[] args) {
+    SpringApplication.run(SampleServiceApplication.class, args);
+  }
+}
