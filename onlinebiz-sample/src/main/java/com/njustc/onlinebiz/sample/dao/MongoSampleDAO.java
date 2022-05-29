1534e0d93cd84c65dfe2e160374b7a7abe59f0f4
stu <wangyue1@smail.nju.edu.cn>
2022-05-30 00:04:00 +0800

    测试一下

--- onlinebiz-sample/src/main/java/com/njustc/onlinebiz/sample/dao/MongoSampleDAO.java
+++
@@ -1,5 +0,0 @@
-package com.njustc.onlinebiz.sample.dao;
-
-public class MongoSampleDAO implements SampleDAO {
-  // TODO: 实现MongoSampleDAO的方法
-}
--- onlinebiz-sample/src/main/java/com/njustc/onlinebiz/sample/dao/SampleDAO.java
+++
@@ -1,5 +0,0 @@
-package com.njustc.onlinebiz.sample.dao;
-
-public interface SampleDAO {
-  // TODO: 在此定义接口方法
-}
