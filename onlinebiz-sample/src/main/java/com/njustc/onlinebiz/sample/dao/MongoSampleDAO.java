880bdebf418386d6dbaf923a3c92fea9adf8244a
syhien <syhien@outlook.at>
2022-06-02 16:35:56 +0800

    修正缺少`@Component`注解的无法自动装配的错误

--- onlinebiz-sample/src/main/java/com/njustc/onlinebiz/sample/dao/MongoSampleDAO.java
+++ onlinebiz-sample/src/main/java/com/njustc/onlinebiz/sample/dao/MongoSampleDAO.java
@@ -7,7 +7,9 @@ import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.stereotype.Component;
 
+@Component
 public class MongoSampleDAO implements SampleDAO {
 
   private static final String COLLECTION_NAME = "sampleCollection";
