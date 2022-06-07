658845f147c4da59dc727851a306d71a3969442a
syhien <syhien@outlook.at>
2022-06-07 17:51:14 +0800

    测试方案dao层去除自己的状态相关

--- onlinebiz-test/src/main/java/com/njustc/onlinebiz/test/dao/scheme/SchemeDAO.java
+++ onlinebiz-test/src/main/java/com/njustc/onlinebiz/test/dao/scheme/SchemeDAO.java
@@ -2,7 +2,6 @@ package com.njustc.onlinebiz.test.dao.scheme;
 
 import com.njustc.onlinebiz.common.model.test.scheme.Scheme;
 import com.njustc.onlinebiz.common.model.test.scheme.SchemeContent;
-import com.njustc.onlinebiz.common.model.test.scheme.SchemeStatus;
 
 public interface SchemeDAO {
     Scheme insertScheme(Scheme scheme);
@@ -10,8 +9,6 @@ public interface SchemeDAO {
     Scheme findSchemeById(String schemeId);
 
     boolean updateContent(String schemeId, SchemeContent content);
-
-    boolean updateStatus(String schemeId, SchemeStatus status);
 
     boolean deleteScheme(String schemeId);
 }
