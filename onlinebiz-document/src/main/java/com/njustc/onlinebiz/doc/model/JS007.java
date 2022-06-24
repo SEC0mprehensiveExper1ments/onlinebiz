78609b94abaff7468d350c965f4f2cba84751162
Xianhao-Dai <438081108@qq.com>
2022-06-24 15:32:07 +0800

    更改project文件，修改了小bug

---
+++ onlinebiz-document/src/main/java/com/njustc/onlinebiz/doc/model/JS007.java
@@ -0,0 +1,15 @@
+package com.njustc.onlinebiz.doc.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Accessors(chain = true)
+public class JS007 {
+    /* 子项先从左往右，从上至下，递增 */
+
+}
