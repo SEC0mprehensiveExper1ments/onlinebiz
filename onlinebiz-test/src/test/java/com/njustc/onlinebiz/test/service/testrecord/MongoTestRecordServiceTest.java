1b062c30efd6744847afb9e792b6ab48393ae612
zhangshimao <1345168596@qq.com>
2022-06-25 16:54:07 +0800

    重新编写了TestIssue的测试代码

---
+++ onlinebiz-test/src/test/java/com/njustc/onlinebiz/test/service/testrecord/MongoTestRecordServiceTest.java
@@ -0,0 +1,24 @@
+package com.njustc.onlinebiz.test.service.testrecord;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MongoTestRecordServiceTest {
+
+    @Test
+    void createTestRecordList() {
+    }
+
+    @Test
+    void findTestRecordList() {
+    }
+
+    @Test
+    void updateTestRecordList() {
+    }
+
+    @Test
+    void removeTestRecordList() {
+    }
+}
