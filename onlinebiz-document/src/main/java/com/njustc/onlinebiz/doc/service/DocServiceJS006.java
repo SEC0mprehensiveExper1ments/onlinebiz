e41fa2663364e2a1d195578365924e1a9fee82ce
Johnny J <1149224994@qq.com>
2022-06-29 16:13:09 +0800

    完善一下JS006的格式

--- onlinebiz-document/src/main/java/com/njustc/onlinebiz/doc/service/DocServiceJS006.java
+++ onlinebiz-document/src/main/java/com/njustc/onlinebiz/doc/service/DocServiceJS006.java
@@ -1,32 +1,21 @@
 package com.njustc.onlinebiz.doc.service;
 
 import com.itextpdf.text.*;
-import com.itextpdf.text.pdf.BaseFont;
-import com.itextpdf.text.pdf.PdfPCell;
-import com.itextpdf.text.pdf.PdfPTable;
-import com.itextpdf.text.pdf.PdfWriter;
-import com.njustc.onlinebiz.common.model.Role;
+import com.itextpdf.text.pdf.*;
 import com.njustc.onlinebiz.common.model.test.scheme.Modification;
-import com.njustc.onlinebiz.common.model.test.scheme.Scheme;
 import com.njustc.onlinebiz.doc.dao.OSSProvider;
-import com.njustc.onlinebiz.doc.exception.DownloadDAOFailureException;
-import com.njustc.onlinebiz.doc.exception.DownloadNotFoundException;
-import com.njustc.onlinebiz.doc.exception.DownloadPermissionDeniedException;
 import com.njustc.onlinebiz.doc.model.JS006;
 import com.njustc.onlinebiz.doc.util.HeaderFooter;
 import com.njustc.onlinebiz.doc.util.ItextUtils;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.*;
 import java.util.List;
-import java.util.Arrays;
 
 @Service
 public class DocServiceJS006 {
@@ -67,12 +56,12 @@ public class DocServiceJS006 {
             File file = new File(pdfPath.replaceAll("\\\\", "/"));
 //            System.out.println("file: " + file);
             PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream(file));
-//            System.out.println("Success");
+
             // 2.5 添加页眉/页脚
             String header = "NST－04－JS006－2011";
             String[] footer = new String[]{"第 ", " 页，共 ", " 页"};
             int headerToPage = 100;
-            int footerFromPage = 1;
+            int footerFromPage = 2;
             boolean isHaderLine = true;
             boolean isFooterLine = false;
             float[] headerLoc = new float[]{document.right() - 5, document.top() + 15};
@@ -83,6 +72,7 @@ public class DocServiceJS006 {
                     headerLoc, footerLoc, headLineOff, footLineOff));
             // 3.打开文档
             document.open();
+
             // 4.向文档中添加内容
             generatePage(document);
             // 5.关闭文档
@@ -226,39 +216,41 @@ public class DocServiceJS006 {
         directory.add(
                 new Phrase(
                         ""
-                                + "1 引言………………………………………………………………………………………………1\n"
-                                + "     1.1标识………………………………………………………………………………………1\n"
-                                + "     1.2系统概述…………………………………………………………………………………1\n"
-                                + "     1.3文档概述…………………………………………………………………………………1\n"
-                                + "     1.4基线………………………………………………………………………………………1\n"
-                                + "2 引用文件…………………………………………………………………………………………1\n"
-                                + "3 软件测试环境……………………………………………………………………………………1\n"
-                                + "     3.1硬件………………………………………………………………………………………1\n"
-                                + "     3.2软件………………………………………………………………………………………1\n"
-                                + "     3.3其他………………………………………………………………………………………1\n"
-                                + "     3.4参与组织…………………………………………………………………………………1\n"
-                                + "     3.5人员………………………………………………………………………………………2\n"
-                                + "4 计划………………………………………………………………………………………………2\n"
-                                + "     4.1总体设计…………………………………………………………………………………2\n"
-                                + "           4.1.1测试级别………………………………………………………………………2\n"
-                                + "           4.1.2测试类别………………………………………………………………………2\n"
-                                + "           4.1.3一般测试条件…………………………………………………………………2\n"
-                                + "     4.2计划执行的测试…………………………………………………………………………2\n"
-                                + "     4.3测试用例…………………………………………………………………………………2\n"
-                                + "5 测试进度表………………………………………………………………………………………2\n"
-                                + "6 需求的可追踪性…………………………………………………………………………………3\n"
+                                + "1 引言………………………………………………………………………………………………4\n"
+                                + "     1.1标识………………………………………………………………………………………4\n"
+                                + "     1.2系统概述…………………………………………………………………………………4\n"
+                                + "     1.3文档概述…………………………………………………………………………………4\n"
+                                + "     1.4基线………………………………………………………………………………………4\n"
+                                + "2 引用文件…………………………………………………………………………………………4\n"
+                                + "3 软件测试环境……………………………………………………………………………………4\n"
+                                + "     3.1硬件………………………………………………………………………………………4\n"
+                                + "     3.2软件………………………………………………………………………………………4\n"
+                                + "     3.3其他………………………………………………………………………………………4\n"
+                                + "     3.4参与组织…………………………………………………………………………………5\n"
+                                + "     3.5人员………………………………………………………………………………………5\n"
+                                + "4 计划………………………………………………………………………………………………5\n"
+                                + "     4.1总体设计…………………………………………………………………………………5\n"
+                                + "           4.1.1测试级别………………………………………………………………………5\n"
+                                + "           4.1.2测试类别………………………………………………………………………5\n"
+                                + "           4.1.3一般测试条件…………………………………………………………………5\n"
+                                + "     4.2计划执行的测试…………………………………………………………………………5\n"
+                                + "     4.3测试用例…………………………………………………………………………………5\n"
+                                + "5 测试进度表………………………………………………………………………………………6\n"
+                                + "6 需求的可追踪性…………………………………………………………………………………6\n"
                         , normal5song));
         document.add(directoryTitle);
         document.add(directory);
 
         document.newPage();
 
+//        Chapter chapter1 = new Chapter(new Paragraph("第1单元  ", normal5song),1);
         Paragraph YinYan = new Paragraph("1 引言", bold2song);
         YinYan.setSpacingBefore(80f); // 设置段落上空白
         //YinYan.setSpacingAfter(30f); // 设置段落下空白
         YinYan.setAlignment(0); // 设置文字居中 0靠左   1，居中     2，靠右
         document.add(YinYan);
 
+//        Section section1_1 = chapter1.addSection(new Paragraph("第1节",normal5song));
         Paragraph BiaoShi = new Paragraph("1.1 标识", bold3song);
         BiaoShi.setSpacingBefore(20f); // 设置段落上空白
         //BiaoShi.setSpacingAfter(30f); // 设置段落下空白
@@ -270,6 +262,7 @@ public class DocServiceJS006 {
         BiaoShiContent.setAlignment(0); // 设置文字居中 0靠左   1，居中     2，靠右
         document.add(BiaoShiContent);
 
+//        Section section1_2 = chapter1.addSection(new Paragraph("第2节",normal5song));
         Paragraph XiTongGaiShu = new Paragraph("1.2 系统概述", bold3song);
         XiTongGaiShu.setSpacingBefore(20f); // 设置段落上空白
         //BiaoShi.setSpacingAfter(30f); // 设置段落下空白
@@ -281,6 +274,7 @@ public class DocServiceJS006 {
         XiTongGaiShuContent.setAlignment(0); // 设置文字居中 0靠左   1，居中     2，靠右
         document.add(XiTongGaiShuContent);
 
+//        Section section1_3 = chapter1.addSection(new Paragraph("第3节",normal5song));
         Paragraph WenDangGaiShu = new Paragraph("1.3 文档概述", bold3song);
         WenDangGaiShu.setSpacingBefore(20f); // 设置段落上空白
         //BiaoShi.setSpacingAfter(30f); // 设置段落下空白
@@ -292,6 +286,7 @@ public class DocServiceJS006 {
         WenDangGaiShuContent.setAlignment(0); // 设置文字居中 0靠左   1，居中     2，靠右
         document.add(WenDangGaiShuContent);
 
+//        Section section1_4 = chapter1.addSection(new Paragraph("第4节",normal5song));
         Paragraph JiXian = new Paragraph("1.4 基线", bold3song);
         JiXian.setSpacingBefore(20f); // 设置段落上空白
         //BiaoShi.setSpacingAfter(30f); // 设置段落下空白
@@ -303,6 +298,7 @@ public class DocServiceJS006 {
         JiXianContent.setAlignment(0); // 设置文字居中 0靠左   1，居中     2，靠右
         document.add(JiXianContent);
 
+//        Chapter chapter2 = new Chapter(new Paragraph("第2单元  ", normal5song),2);
         Paragraph YinYongWenJian = new Paragraph("2 引用文件", bold2song);
         YinYongWenJian.setSpacingBefore(20f); // 设置段落上空白
         YinYongWenJian.setAlignment(0); // 设置文字居中 0靠左   1，居中     2，靠右
@@ -313,11 +309,13 @@ public class DocServiceJS006 {
         YinYongWenJianContent.setAlignment(0); // 设置文字居中 0靠左   1，居中     2，靠右
         document.add(YinYongWenJianContent);
 
+//        Chapter chapter3 = new Chapter(new Paragraph("第3单元  ", normal5song),3);
         Paragraph HuanJing = new Paragraph("3 软件测试环境", bold2song);
         HuanJing.setSpacingBefore(20f); // 设置段落上空白
         HuanJing.setAlignment(0); // 设置文字居中 0靠左   1，居中     2，靠右
         document.add(HuanJing);
 
+//        Section section3_1 = chapter3.addSection(new Paragraph("第1节",normal5song));
         Paragraph YingJian = new Paragraph("3.1 硬件", bold3song);
         YingJian.setSpacingBefore(20f); // 设置段落上空白
         YingJian.setAlignment(0); // 设置文字居中 0靠左   1，居中     2，靠右
@@ -328,6 +326,7 @@ public class DocServiceJS006 {
         YingJianContent.setAlignment(0); // 设置文字居中 0靠左   1，居中     2，靠右
         document.add(YingJianContent);
 
+//        Section section3_2 = chapter3.addSection(new Paragraph("第2节",normal5song));
         Paragraph RuanJian = new Paragraph("3.2 软件", bold3song);
         RuanJian.setSpacingBefore(20f); // 设置段落上空白
         RuanJian.setAlignment(0); // 设置文字居中 0靠左   1，居中     2，靠右
@@ -338,6 +337,7 @@ public class DocServiceJS006 {
         RuanJianContent.setAlignment(0); // 设置文字居中 0靠左   1，居中     2，靠右
         document.add(RuanJianContent);
 
+//        Section section3_3 = chapter3.addSection(new Paragraph("第3节",normal5song));
         Paragraph QiTa = new Paragraph("3.3 其他", bold3song);
         QiTa.setSpacingBefore(20f); // 设置段落上空白
         QiTa.setAlignment(0); // 设置文字居中 0靠左   1，居中     2，靠右
@@ -348,6 +348,7 @@ public class DocServiceJS006 {
         QiTaContent.setAlignment(0); // 设置文字居中 0靠左   1，居中     2，靠右
         document.add(QiTaContent);
 
+//        Section section3_4 = chapter3.addSection(new Paragraph("第4节",normal5song));
         Paragraph CanYuZuZhi = new Paragraph("3.4 参与组织", bold3song);
         CanYuZuZhi.setSpacingBefore(20f); // 设置段落上空白
         CanYuZuZhi.setAlignment(0); // 设置文字居中 0靠左   1，居中     2，靠右
@@ -358,6 +359,7 @@ public class DocServiceJS006 {
         CanYuZuZhiContent.setAlignment(0); // 设置文字居中 0靠左   1，居中     2，靠右
         document.add(CanYuZuZhiContent);
 
+//        Section section3_5 = chapter3.addSection(new Paragraph("第5节",normal5song));
         Paragraph RenYuan = new Paragraph("3.5 人员", bold3song);
         RenYuan.setSpacingBefore(20f); // 设置段落上空白
         RenYuan.setAlignment(0); // 设置文字居中 0靠左   1，居中     2，靠右
@@ -368,6 +370,7 @@ public class DocServiceJS006 {
         RenYuanContent.setAlignment(0); // 设置文字居中 0靠左   1，居中     2，靠右
         document.add(RenYuanContent);
 
+//        Chapter chapter4 = new Chapter(new Paragraph("第4单元  ", normal5song),4);
         Paragraph JiHua = new Paragraph("4 计划", bold2song);
         JiHua.setSpacingBefore(20f); // 设置段落上空白
         JiHua.setAlignment(0); // 设置文字居中 0靠左   1，居中     2，靠右
@@ -378,11 +381,13 @@ public class DocServiceJS006 {
         JiHuaContent.setAlignment(0); // 设置文字居中 0靠左   1，居中     2，靠右
         document.add(JiHuaContent);
 
+//        Section section4_1 = chapter4.addSection(new Paragraph("第1节",normal5song));
         Paragraph ZongTiSheJi = new Paragraph("4.1 总体设计", bold3song);
         ZongTiSheJi.setSpacingBefore(20f); // 设置段落上空白
         ZongTiSheJi.setAlignment(0); // 设置文字居中 0靠左   1，居中     2，靠右
         document.add(ZongTiSheJi);
 
+//        Section section4_1_1 = chapter4.addSection(new Paragraph("第1题",normal5song));
         Paragraph CeShiJiBie = new Paragraph("4.1.1 测试级别", bold3song);
         CeShiJiBie.setSpacingBefore(20f); // 设置段落上空白
         CeShiJiBie.setAlignment(0); // 设置文字居中 0靠左   1，居中     2，靠右
@@ -393,6 +398,7 @@ public class DocServiceJS006 {
         CeShiJiBieContent.setAlignment(0); // 设置文字居中 0靠左   1，居中     2，靠右
         document.add(CeShiJiBieContent);
 
+//        Section section4_1_2 = chapter4.addSection(new Paragraph("第2题",normal5song));
         Paragraph CeShiLeiBie = new Paragraph("4.1.2 测试类别", bold3song);
         CeShiLeiBie.setSpacingBefore(20f); // 设置段落上空白
         CeShiLeiBie.setAlignment(0); // 设置文字居中 0靠左   1，居中     2，靠右
@@ -403,6 +409,7 @@ public class DocServiceJS006 {
         CeShiLeiBieContent.setAlignment(0); // 设置文字居中 0靠左   1，居中     2，靠右
         document.add(CeShiLeiBieContent);
 
+//        Section section4_1_3 = chapter4.addSection(new Paragraph("第3题",normal5song));
         Paragraph YiBanCeShiTiaoJian = new Paragraph("4.1.3 一般测试条件", bold3song);
         YiBanCeShiTiaoJian.setSpacingBefore(20f); // 设置段落上空白
         YiBanCeShiTiaoJian.setAlignment(0); // 设置文字居中 0靠左   1，居中     2，靠右
@@ -413,6 +420,7 @@ public class DocServiceJS006 {
         YiBanCeShiTiaoJianContent.setAlignment(0); // 设置文字居中 0靠左   1，居中     2，靠右
         document.add(YiBanCeShiTiaoJianContent);
 
+//        Section section4_2 = chapter4.addSection(new Paragraph("第2节",normal5song));
         Paragraph JiHuaZhiXingDeCeShi = new Paragraph("4.2 计划执行的测试", bold3song);
         JiHuaZhiXingDeCeShi.setSpacingBefore(20f); // 设置段落上空白
         JiHuaZhiXingDeCeShi.setAlignment(0); // 设置文字居中 0靠左   1，居中     2，靠右
@@ -423,6 +431,7 @@ public class DocServiceJS006 {
         JiHuaZhiXingDeCeShiContent.setAlignment(0); // 设置文字居中 0靠左   1，居中     2，靠右
         document.add(JiHuaZhiXingDeCeShiContent);
 
+//        Section section4_3 = chapter4.addSection(new Paragraph("第3节",normal5song));
         Paragraph CeShiYongLi = new Paragraph("4.3 测试用例", bold3song);
         CeShiYongLi.setSpacingBefore(20f); // 设置段落上空白
         CeShiYongLi.setAlignment(0); // 设置文字居中 0靠左   1，居中     2，靠右
@@ -433,6 +442,7 @@ public class DocServiceJS006 {
         CeShiYongLiContent.setAlignment(0); // 设置文字居中 0靠左   1，居中     2，靠右
         document.add(CeShiYongLiContent);
 
+//        Chapter chapter5 = new Chapter(new Paragraph("第5单元  ", normal5song),5);
         Paragraph CeShiJinDuBiao = new Paragraph("5 测试进度表", bold2song);
         CeShiJinDuBiao.setSpacingBefore(20f); // 设置段落上空白
         CeShiJinDuBiao.setAlignment(0); // 设置文字居中 0靠左   1，居中     2，靠右
@@ -474,6 +484,7 @@ public class DocServiceJS006 {
         CeShiJinDuTable.addCell(ItextUtils.createCell(JS006Json.getPingGuCeShi().getEndDate().toString(), normal5song, Element.ALIGN_LEFT, 1, 2, paddings3, borderWidth));
         document.add(CeShiJinDuTable);
 
+//        Chapter chapter6 = new Chapter(new Paragraph("第6单元  ", normal5song),6);
         Paragraph XuQiuKeZhuiZongXing = new Paragraph("6 需求的可追踪性", bold2song);
         XuQiuKeZhuiZongXing.setSpacingBefore(20f); // 设置段落上空白
         XuQiuKeZhuiZongXing.setAlignment(0); // 设置文字居中 0靠左   1，居中     2，靠右
@@ -484,4 +495,26 @@ public class DocServiceJS006 {
         XuQiuKeZhuiZongXingContent.setAlignment(0); // 设置文字居中 0靠左   1，居中     2，靠右
         document.add(XuQiuKeZhuiZongXingContent);
     }
+
+    //页码监听
+//    private static class ContentEvent extends PdfPageEventHelper {
+//
+//        private int page;
+//        Map<String, Integer> index = new LinkedHashMap<String, Integer>();
+//
+//        @Override
+//        public void onStartPage (PdfWriter writer, Document document) {
+//            page++;
+//        }
+//
+//        @Override
+//        public void onChapter (PdfWriter writer, Document document, float paragraphPosition, Paragraph title) {
+//            index.put(title.getContent(), page);
+//        }
+//
+//        @Override
+//        public void onSection (PdfWriter writer, Document document, float paragraphPosition, int depth, Paragraph title) {
+//            onChapter(writer, document, paragraphPosition, title);
+//        }
+//    }
 }
