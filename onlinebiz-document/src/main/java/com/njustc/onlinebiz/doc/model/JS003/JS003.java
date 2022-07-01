253fff50fcd91c78fe4759fe875e7e9f1a151594
Johnny J <1149224994@qq.com>
2022-07-01 20:50:09 +0800

    JS003增加字段的非空性检查

--- onlinebiz-document/src/main/java/com/njustc/onlinebiz/doc/model/JS003/JS003.java
+++ onlinebiz-document/src/main/java/com/njustc/onlinebiz/doc/model/JS003/JS003.java
@@ -12,105 +12,75 @@ import lombok.experimental.Accessors;
 import java.util.ArrayList;
 import java.util.List;
 
-//@Data
-//@NoArgsConstructor
-//@AllArgsConstructor
-//@Accessors(chain = true)
-//class ZiGongNeng {
-//  private String inputZiGongNengMingCheng;
-//  private String inputGongNengShuoMing;
-//
-//  public String getInputZiGongNengMingCheng() {
-//    return inputZiGongNengMingCheng;
-//  }
-//
-//  public String getInputGongNengShuoMing() {
-//    return inputGongNengShuoMing;
-//  }
-//}
-
-//@Data
-//@NoArgsConstructor
-//@AllArgsConstructor
-//@Accessors(chain = true)
-//public
-//class GongNeng {
-//  private String inputGongNengMingCheng;
-//  private List<ZiGongNeng> inputZiGongNeng;
-//
-//  public String getInputGongNengMingCheng() {
-//    return inputGongNengMingCheng;
-//  }
-//
-//  public List<ZiGongNeng> getInputZiGongNeng() {
-//    return inputZiGongNeng;
-//  }
-//
-//  public int getZiGongNengSum() {
-//    return inputZiGongNeng.size();
-//  }
-//}
-
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Accessors(chain = true)
 public class JS003 {
-  private String inputRuanJianMingCheng = "";
-  private String inputBanBenHao = "";
-  private List<GongNeng> inputRuanJianGongNengXiangMu = new ArrayList<>();
+    private String inputRuanJianMingCheng = "";
+    private String inputBanBenHao = "";
+    private List<GongNeng> inputRuanJianGongNengXiangMu = new ArrayList<>();
 
-  public String getInputRuanJianMingCheng() {
-    return inputRuanJianMingCheng;
-  }
-
-  public String getInputBanBenHao() {
-    return inputBanBenHao;
-  }
-
-  public List<GongNeng> getInputRuanJianGongNengXiangMu() {
-    return inputRuanJianGongNengXiangMu;
-  }
-
-  public int getGongNengSum() {
-    return inputRuanJianGongNengXiangMu.size();
-  }
-
-  public int getZiGongNengSum(int index) {
-    return inputRuanJianGongNengXiangMu.get(index).getZiGongNengSum();
-  }
-
-  public String getGongNengName(int index) {
-    return inputRuanJianGongNengXiangMu.get(index).getInputGongNengMingCheng();
-  }
-
-  public String getZiGongNengName(int index, int j) {
-    return inputRuanJianGongNengXiangMu
-        .get(index)
-        .getInputZiGongNeng()
-        .get(j)
-        .getInputZiGongNengMingCheng();
-  }
-
-  public String getZiGongNengShuoMing(int index, int j) {
-    return inputRuanJianGongNengXiangMu
-        .get(index)
-        .getInputZiGongNeng()
-        .get(j)
-        .getInputGongNengShuoMing();
-  }
-
-  public JS003(Entrust entrust) {
-    //
-    Software software = entrust.getContent().getSoftware();
-    this.inputRuanJianMingCheng = software.getName();
-    this.inputBanBenHao = software.getVersion();
-    // 软件功能列表
-    List<Module> modules = software.getModules();
-    for (Module module: modules) {
-      GongNeng gongNeng = new GongNeng(module);
-      this.inputRuanJianGongNengXiangMu.add(gongNeng);
+    public String getInputRuanJianMingCheng() {
+        return inputRuanJianMingCheng;
     }
-  }
+
+    public String getInputBanBenHao() {
+        return inputBanBenHao;
+    }
+
+    public List<GongNeng> getInputRuanJianGongNengXiangMu() {
+        return inputRuanJianGongNengXiangMu;
+    }
+
+    public int getGongNengSum() {
+        return inputRuanJianGongNengXiangMu.size();
+    }
+
+    public int getZiGongNengSum(int index) {
+        return inputRuanJianGongNengXiangMu.get(index).getZiGongNengSum();
+    }
+
+    public String getGongNengName(int index) {
+        return inputRuanJianGongNengXiangMu.get(index).getInputGongNengMingCheng();
+    }
+
+    public String getZiGongNengName(int index, int j) {
+        return inputRuanJianGongNengXiangMu
+                .get(index)
+                .getInputZiGongNeng()
+                .get(j)
+                .getInputZiGongNengMingCheng();
+    }
+
+    public String getZiGongNengShuoMing(int index, int j) {
+        return inputRuanJianGongNengXiangMu
+                .get(index)
+                .getInputZiGongNeng()
+                .get(j)
+                .getInputGongNengShuoMing();
+    }
+
+    public JS003(Entrust entrust) {
+        Software software = entrust.getContent().getSoftware();
+        this.inputRuanJianMingCheng = software.getName();
+        this.inputBanBenHao = software.getVersion();
+        // 软件功能列表
+        List<Module> modules = software.getModules();
+        for (Module module : modules) {
+            GongNeng gongNeng = new GongNeng(module);
+            this.inputRuanJianGongNengXiangMu.add(gongNeng);
+        }
+        //非空性检查
+        if (this.inputRuanJianMingCheng == null) {
+            this.inputRuanJianMingCheng = "";
+        }
+        if (this.inputBanBenHao == null) {
+            this.inputBanBenHao = "";
+        }
+        if (this.inputRuanJianGongNengXiangMu == null) {
+            this.inputRuanJianGongNengXiangMu = new ArrayList<>();
+        }
+    }
 
 }
