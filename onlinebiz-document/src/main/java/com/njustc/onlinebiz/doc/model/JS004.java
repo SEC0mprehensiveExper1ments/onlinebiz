6d65db2734d83acf621e9517515c11bcb113c6f4
Johnny J <1149224994@qq.com>
2022-07-01 17:56:20 +0800

    JS004增加字段的非空性检查

--- onlinebiz-document/src/main/java/com/njustc/onlinebiz/doc/model/JS004.java
+++ onlinebiz-document/src/main/java/com/njustc/onlinebiz/doc/model/JS004.java
@@ -88,6 +88,30 @@ public class JS004 {
     this.inputYiFang0KaiHuYinHang = yiFang.getBankName();
     this.inputYiFang0ZhangHao = yiFang.getAccount();
     this.inputYiFang0YouBian = yiFang.getZipCode();
+    //增加字段的非空性检查
+    if (this.inputXiangMuMingChen == null) this.inputXiangMuMingChen = "";
+    if (this.inputJiaFang == null) this.inputJiaFang = "";
+    if (this.inputYiFang == null) this.inputYiFang = "";
+    if (this.inputQianDingDiDian == null) this.inputQianDingDiDian = "";
+    if (this.inputShouCeRuanJian == null) this.inputShouCeRuanJian = "";
+    if (this.inputCeShiFeiYong == null) this.inputCeShiFeiYong = "";
+    if (this.inputJiaFang0ShouQuanDaiBiao == null) this.inputJiaFang0ShouQuanDaiBiao = "";
+    if (this.inputJiaFang0LianXiRen == null) this.inputJiaFang0LianXiRen = "";
+    if (this.inputJiaFang0TongXunDiZhi == null) this.inputJiaFang0TongXunDiZhi = "";
+    if (this.inputJiaFang0DianHua == null) this.inputJiaFang0DianHua = "";
+    if (this.inputJiaFang0ChuanZhen == null) this.inputJiaFang0ChuanZhen = "";
+    if (this.inputJiaFang0KaiHuYinHang == null) this.inputJiaFang0KaiHuYinHang = "";
+    if (this.inputJiaFang0ZhangHao == null) this.inputJiaFang0ZhangHao = "";
+    if (this.inputJiaFang0YouBian == null) this.inputJiaFang0YouBian = "";
+    if (this.inputYiFang0ShouQuanDaiBiao == null) this.inputYiFang0ShouQuanDaiBiao = "";
+    if (this.inputYiFang0LianXiRen == null) this.inputYiFang0LianXiRen = "";
+    if (this.inputYiFang0TongXunDiZhi == null) this.inputYiFang0TongXunDiZhi = "";
+    if (this.inputYiFang0DianHua == null) this.inputYiFang0DianHua = "";
+    if (this.inputYiFang0ChuanZhen == null) this.inputYiFang0ChuanZhen = "";
+    if (this.inputYiFang0HuMing == null) this.inputYiFang0HuMing = "";
+    if (this.inputYiFang0KaiHuYinHang == null) this.inputYiFang0KaiHuYinHang = "";
+    if (this.inputYiFang0ZhangHao == null) this.inputYiFang0ZhangHao = "";
+    if (this.inputYiFang0YouBian == null) this.inputYiFang0YouBian = "";
   }
 
 }
