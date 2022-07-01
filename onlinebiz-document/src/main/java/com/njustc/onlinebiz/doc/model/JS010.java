44dc30222805def6797a9a97ce089c6a48e08152
Johnny J <1149224994@qq.com>
2022-07-01 17:50:21 +0800

    JS010增加字段的非空性检查

--- onlinebiz-document/src/main/java/com/njustc/onlinebiz/doc/model/JS010.java
+++ onlinebiz-document/src/main/java/com/njustc/onlinebiz/doc/model/JS010.java
@@ -35,20 +35,28 @@ public class JS010 {
         this.inputRuanJianMingChen = reportReview.getSoftwareName();
         this.inputWeiTuoDanWei = reportReview.getPrincipal();
         List<ReportReview.ConclusionRow> conclusions = reportReview.getConclusions();
-        this.inputJianChaJieGuo01 = conclusions.get(0).isPassed() ? "通过" : "不通过";
-        this.inputJianChaJieGuo02 = conclusions.get(1).isPassed() ? "通过" : "不通过";
-        this.inputJianChaJieGuo03 = conclusions.get(2).isPassed() ? "通过" : "不通过";
-        this.inputJianChaJieGuo04 = conclusions.get(3).isPassed() ? "通过" : "不通过";
-        this.inputJianChaJieGuo05 = conclusions.get(4).isPassed() ? "通过" : "不通过";
-        this.inputJianChaJieGuo06 = conclusions.get(5).isPassed() ? "通过" : "不通过";
-        this.inputJianChaJieGuo07 = conclusions.get(6).isPassed() ? "通过" : "不通过";
-        this.inputJianChaJieGuo08 = conclusions.get(7).isPassed() ? "通过" : "不通过";
-        this.inputJianChaJieGuo09 = conclusions.get(8).isPassed() ? "通过" : "不通过";
-        this.inputJianChaJieGuo010 = conclusions.get(9).isPassed() ? "通过" : "不通过";
-        this.inputJianChaJieGuo0111 = conclusions.get(10).isPassed() ? "通过" : "不通过";
-        this.inputJianChaJieGuo0112 = conclusions.get(11).isPassed() ? "通过" : "不通过";
-        this.inputJianChaJieGuo0113 = conclusions.get(12).isPassed() ? "通过" : "不通过";
-        this.inputJianChaJieGuo012 = conclusions.get(13).isPassed() ? "通过" : "不通过";
-
+        if (conclusions != null && conclusions.size() == 14) {
+            this.inputJianChaJieGuo01 = conclusions.get(0).isPassed() ? "通过" : "不通过";
+            this.inputJianChaJieGuo02 = conclusions.get(1).isPassed() ? "通过" : "不通过";
+            this.inputJianChaJieGuo03 = conclusions.get(2).isPassed() ? "通过" : "不通过";
+            this.inputJianChaJieGuo04 = conclusions.get(3).isPassed() ? "通过" : "不通过";
+            this.inputJianChaJieGuo05 = conclusions.get(4).isPassed() ? "通过" : "不通过";
+            this.inputJianChaJieGuo06 = conclusions.get(5).isPassed() ? "通过" : "不通过";
+            this.inputJianChaJieGuo07 = conclusions.get(6).isPassed() ? "通过" : "不通过";
+            this.inputJianChaJieGuo08 = conclusions.get(7).isPassed() ? "通过" : "不通过";
+            this.inputJianChaJieGuo09 = conclusions.get(8).isPassed() ? "通过" : "不通过";
+            this.inputJianChaJieGuo010 = conclusions.get(9).isPassed() ? "通过" : "不通过";
+            this.inputJianChaJieGuo0111 = conclusions.get(10).isPassed() ? "通过" : "不通过";
+            this.inputJianChaJieGuo0112 = conclusions.get(11).isPassed() ? "通过" : "不通过";
+            this.inputJianChaJieGuo0113 = conclusions.get(12).isPassed() ? "通过" : "不通过";
+            this.inputJianChaJieGuo012 = conclusions.get(13).isPassed() ? "通过" : "不通过";
+        }
+        //增加字段的非空性检查
+        if (this.inputRuanJianMingChen == null) {
+            this.inputRuanJianMingChen = "";
+        }
+        if (this.inputWeiTuoDanWei == null) {
+            this.inputWeiTuoDanWei = "";
+        }
     }
 }
